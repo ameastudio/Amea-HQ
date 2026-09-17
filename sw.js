96fc4abad45ac031201e9d30c7e31949d6576653
ameastudio <ameastudio1@gmail.com>
2026-09-17 13:28:33 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='amea-v10-items-catalog',A=['./','./index.html','./styles.css','./app.js','./manifest.json'];
+const C='amea-v11-nav-invoices',A=['./','./index.html','./styles.css','./app.js','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
